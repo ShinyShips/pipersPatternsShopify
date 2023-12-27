df078602d069900bb8b2793655814845f4584e51
Shinyships <xxandynguyenxx@gmail.com>
2023-12-27 17:02:48 -0500

    added netlify, fixed error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import netlify from "@astrojs/netlify/functions";
 
 // https://astro.build/config
 export default defineConfig({
   renderers: ['@astrojs/renderer-react'],
   integrations: [react(), tailwind()],
-    image: {
-    remotePatterns: [{ protocol: "https" }],
-  }
+  image: {
+    remotePatterns: [{
+      protocol: "https"
+    }]
+  },
+  output: "server",
+  adapter: netlify()
 });
