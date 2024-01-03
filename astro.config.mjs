c1ef73777407724b0b7b9c4edc049f370578d344
Shinyships <xxandynguyenxx@gmail.com>
2024-01-02 21:57:59 -0500

    fixing build error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify";
-import { astroImageTools } from "astro-imagetools";
 
 // https://astro.build/config
 export default defineConfig({
   renderers: ['@astrojs/renderer-react'],
-  integrations: [react(), tailwind(), astroImageTools],
+  integrations: [react(), tailwind()],
   output: "server",
   adapter: netlify()
 });
